c624f221c07eafff90b1305578a414d323fb3508
Maxmengt <mengt2012@gmail.com>
2017-01-20 00:54:55 +0800

    add micro_allocator.h

---
+++ MicroSTL/MicroSTL/micro_allocator.h
@@ -0,0 +1,63 @@
+#ifndef _MICRO_ALLOCATOR_H
+#define _MICRO_ALLOCATOR_H
+
+namespace Micro_STL {
+	template<class T>
+	class Allocator {
+	public:
+		static T* allocate();
+		static T* allocate(size_t n);
+		static void deallocate(T* place_address);
+		static void deallocate(T* place_address, size_t n);
+		
+		static void construct(T* place_address);
+		static void construct(T* place_address, const T& value);
+		static void destroy(T* obj);
+		static void destroy(T* first, T* last);
+	};
+
+	template<class T>
+	T* Allocator::allocate() {
+		return Alloc::allocate(sizeof(T));
+	}
+	
+	template<class T>
+	T* Allocator::allocate(size_t n) {
+		return Alloc::allocate(sizeof(T) * n);
+	}
+
+	template<class T>
+	void Allocator::deallocate(T* place_address) {
+		Alloc::deallocate(place_address);
+	}
+
+	template<class T>
+	void Allocator::deallocate(T* place_address, size_t n) {
+		Alloc::deallocate(place_address, sizeof(T) * n);
+	}
+
+	template<class T>
+	void Allocator::construct(T* place_address) {
+		new (place_address) T();
+	}
+
+	template<class T>
+	void Allocator::construct(T* place_address, const T& value) {
+		new (place_address) T(value);
+	}
+
+	template<class T>
+	void Allocator::destroy(T* obj) {
+		obj->~T();
+	}
+
+	template<class T>
+	void Allocator::destroy(T* first, T* last) {
+		// has_trivial_destructor ?
+		for( ; first != last; ++ first ) {
+			first->~T();
+		}
+	}
+}
+
+#endif // !_MICRO_ALLOCATOR_H
