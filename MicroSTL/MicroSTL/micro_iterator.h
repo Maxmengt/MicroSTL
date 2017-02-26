3a9a8dc44b4212bf2a8bed21bf3b19f3d5c3be84
Maxmengt <mengt2012@gmail.com>
2017-02-27 01:17:05 +0800

    keep on achieving micro_iterator

--- MicroSTL/MicroSTL/micro_iterator.h
+++ MicroSTL/MicroSTL/micro_iterator.h
@@ -1,16 +1,41 @@
 #ifndef _MICRO_ITERATOR_H_
 #define _MICRO_ITERATOR_H_
 
+#include <cstddef>
+
 namespace MicroSTL {
 	template<class I>
 	struct iterator_traits {
-		typedef typename I::value_type value_type;
+		typedef typename I::iterator_category	iterator_category;
+		typedef typename I::value_type			value_type;
+		typedef typename I::difference_type		difference_type;
+		typedef typename I::pointer				pointer;
+		typedef typename I::reference			reference;
 	};
 	
 	template<class T>
 	struct iterator_traits<T*> {
-		typedef T value_type;
+		typedef random_access_iterator_tag		iterator_category;
+		typedef T								value_type;
+		typedef ptrdiff_t						difference_type;
+		typedef T*								pointer;
+		typedef T&								reference;
 	};
+
+	template<class T>
+	struct iterator_traits<const T*> {
+		typedef random_access_iterator_tag		iterator_category;
+		typedef T								value_type;
+		typedef ptrdiff_t						difference_type;
+		typedef const T*						pointer;
+		typedef const T&						reference;
+	};
+
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 }
 
 #endif // !_MICRO_ITERATOR_H_
