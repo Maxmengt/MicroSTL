ecfdc57291a1899770abf8f1f46e516c047547ca
Maxmengt <mengt2012@gmail.com>
2017-02-27 10:59:29 +0800

    remove some redundant

--- MicroSTL/MicroSTL/micro_iterator.h
+++ MicroSTL/MicroSTL/micro_iterator.h
@@ -84,8 +84,7 @@ namespace MicroSTL {
 	template<class InputIterator>
 	inline typename iterator_traits<InputIterator>::difference_type
 	distance(InputIterator first, InputIterator last) {
-		typedef typename iterator_traits<InputIterator>::iterator_category category;
-		return __distance(first, last, category());
+		return __distance(first, last, iterator_category());
 	}
 
 	template<class InputIterator, class Distance>
