f331068a228cfb74108c65ebe313b771ed6ec91d
Maxmengt <mengt2012@gmail.com>
2017-02-28 14:32:05 +0800

    micro_type_traits:add new

--- MicroSTL/MicroSTL/micro_type_traits.h
+++ MicroSTL/MicroSTL/micro_type_traits.h
@@ -1,6 +1,19 @@
 #ifndef _MICRO_TYPE_TRAITS_H_
 #define _MICRO_TYPE_TRAITS_H_
 
+namespace MicroSTL {
+	struct __true_type {};
+	struct __false_type {};
 
+	template<class type>
+	struct __type_traits {
+		typedef __true_type		this_dummy_member_must_be_first;
+		typedef __false_type	has_trivial_default_constructor;
+		typedef __false_type	has_trivial_copy_constructor;
+		typedef __false_type	has_trivial_assignment_operator;
+		typedef __false_type	has_trivial_destructor;
+		typedef __false_type	is_POD_type;
+	};
+}
 
 #endif // !_MICRO_TYPE_TRAITS_H_
